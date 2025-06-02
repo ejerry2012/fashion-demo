4fe5d71d4d0d127d7712b6b74b55886da31e8959
ejerry2012 <143281532+ejerry2012@users.noreply.github.com>
2025-06-02 11:37:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-Close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener()
