20eeaef84221467f297c15877eed5d803faf301c
rowleks <momohrowland@gmail.com>
2025-06-02 12:20:17 +0100

    fixed hamburger bug

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
-const menuCloseButton = document.querySelector("#menu-Close-button");
+const menuCloseButton = document.querySelector("#menu-close-button"); // You had a typo here, it shouldn't have been #menuCloseButton
 
-menuOpenButton.addEventListener("click", () => {
-    // Toggle mobile menu visibility
-    document.body.classList.toggle("show-mobile-menu");
+menuOpenButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevent page reload
+  document.body.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener()
+menuCloseButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevent page reload
+  document.body.classList.remove("show-mobile-menu");
+});
